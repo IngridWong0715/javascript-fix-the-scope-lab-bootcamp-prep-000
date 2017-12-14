20b41e09a56a0d006ed0c0e10d80c83775d7a9e1
Ingrid Wong <yw340@georgetown.edu>
2017-12-14 02:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ function add2(n) {
   return n + two;
 }
 
+
+
+/*  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof window.funkyFunction()).toEqual('function')
+    })
+  })
+
+  describe('theFunk', function() {
+    it('is "FUNKY!"', function() {
+      expect(window.theFunk).toEqual('FUNKY!')
+    })
+  })*/
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
@@ -23,5 +36,5 @@ var funkyFunction = function() {
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction();
+
+var theFunk = funkyFunction()();
